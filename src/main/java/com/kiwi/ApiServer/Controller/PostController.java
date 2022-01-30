280a8d1ff3ec69d11a288cb76678e62c5979a33c
Pororo-droid <yjwsis@kookmin.ac.kr>
2022-01-31 01:46:03 +0900

    [ADD] Upload File

--- src/main/java/com/kiwi/ApiServer/Controller/PostController.java
+++ src/main/java/com/kiwi/ApiServer/Controller/PostController.java
@@ -1,8 +1,34 @@
 package com.kiwi.ApiServer.Controller;
 
+import com.kiwi.ApiServer.DTO.ResponseMessage;
+import com.kiwi.ApiServer.Service.FileStorageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
 
 @RestController
 public class PostController {
+    @Autowired
+    FileStorageService storageService;
 
+    @PostMapping(value = "upload")
+    public ResponseEntity<ResponseMessage> uploadFile(@RequestParam("file") MultipartFile file){
+        String message = "";
+        try{
+            storageService.save(file);
+
+            message = "Uploaded the file sucessfully: " + file.getOriginalFilename();
+            return ResponseEntity.status(HttpStatus.OK).body(new ResponseMessage(message));
+        } catch(Exception e){
+            message = "Could not upload the file: " + file.getOriginalFilename();
+            return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseMessage(message));
+        }
+    }
 }
--- src/main/java/com/kiwi/ApiServer/DAO/SQLDAO.java
+++ src/main/java/com/kiwi/ApiServer/DAO/SQLDAO.java
@@ -5,9 +5,11 @@ import java.sql.*;
 public class SQLDAO {
     private static Connection conn;
     private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
-//    private static final String URL = "JDBC:MYSQL://localhost:3306/?useSSL=false&serverTimezone=Asia/Seoul";
+//
+    private static final String URL = "JDBC:MYSQL://localhost:3306/?useSSL=false&serverTimezone=Asia/Seoul";
     private static final String USER = "root";
-//    private static final String PW = "";
+//
+    private static final String PW = "";
 
     public SQLDAO() throws Exception{
         conn = DriverManager.getConnection(URL,USER,PW);
